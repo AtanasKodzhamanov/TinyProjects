3187e0accc4d3afe91ddfc6c5ab7f305ab6fc818
kodzh <39922895+kodzh@users.noreply.github.com>
2022-06-26 02:06:25 +0100

    .

---
+++ DailyBrief.py
@@ -0,0 +1,31 @@
+from this import d
+
+
+class DailyBrief(){
+
+    __init__(self):
+        
+d
+}
+
+#def AddQuote(content)
+#def EmailScheduler(content,frequency)
+#def WriteToExcel()
+#def WriteToEmail()
+
+#def GetWeather()
+
+#def WorkoutSchedule():
+
+
+    
+
+#def GetNews()
+#def GetStocks()
+#def DailyStreak():
+    #shows if you have been hitting your daily goals:
+        #add remove goal
+        #track streak
+        #track days since failure
+        #keep best result ever and how long it has been since you achieved it
+        #show some basic analytics. You have x success in this month compared to last. Or you trending down.
