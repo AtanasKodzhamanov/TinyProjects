6e7734c75cc116a1b145215d9ec397b1d1322445
atanas <kodzhamanov@gmail.com>
2022-12-09 06:51:01 +0000

    Monopoly skeleton

---
+++ MonopolyClone/BoardSetup.py
@@ -0,0 +1,55 @@
+import random 
+
+#initialize the properties
+colors = ["Red", "Orange", "Yellow", "Green", "Blue", "Purple", "Pink", "Brown", "Gray", "Black"]
+
+street_names = ["Maple Ave", "Main St", "Washington Blvd", "Park Ave", "Elm St", "First St", "Second St", "Third St", "Fourth Ave", "Fifth Ave", "Sixth Ave", "Seventh Ave", "Eighth Ave", "Ninth Ave", "Tenth Ave", "Eleventh Ave", "Twelfth Ave", "Thirteenth Ave", "Fourteenth Ave", "Fifteenth Ave", "Sixteenth Ave", "Seventeenth Ave", "Eighteenth Ave", "Nineteenth Ave", "Twenty-First Ave", "Twenty-Second Ave", "Twenty-Third Ave", "Twenty-Fourth Ave", "Twenty-Fifth Ave"]
+
+streets = {}
+
+for i, street_name in enumerate(street_names):
+    color = random.choice(colors)
+    streets[street_name] = {
+        "color": color,
+        "price": (i + 1) * 100,
+        "rent": (i + 1) * 5,
+        "house_multiplier": random.uniform(1.2, 1.4),
+        "houses": 0,
+        "owner": None
+    }
+
+# Ensure that there are between 2 and 4 repetitions of each color
+for color in colors:
+    color_count = sum(1 for street in streets.values() if street["color"] == color)
+    if color_count < 2 or color_count > 4:
+        # Find a street with a different color and reassign its color
+        for street in streets.values():
+            if street["color"] != color:
+                street["color"] = color
+                break
+
+organization_names = ["Department of Redundant Regulations", "Bureau of Endless Paperwork", "Office of Inefficient Procedures", "Agency of Unnecessary Complications"]
+
+organizations = {}
+
+for organization_name in organization_names:
+    organizations[organization_name] = {
+        "reward": 0,
+        "punishment": 0,
+        "effect_list": []
+    }
+
+
+def assign_positions():
+    # Create an empty array with the same length as the number of organizations and streets
+    positions = [None] * (len(organizations) + len(streets))
+
+    # Assign each organization a position in the array
+    for i, organization_name in enumerate(organizations.keys()):
+        positions[i] = (organization_name, organizations[organization_name])
+
+    # Assign each street a position in the array
+    for i, street_name in enumerate(streets.keys()):
+        positions[len(organizations) + i] = (street_name, streets[street_name])
+    print(positions)
+    return positions
---
+++ MonopolyClone/MonopolyClone.py
@@ -0,0 +1,63 @@
+import random
+from BoardSetup import assign_positions
+
+# Set up the board, place properties and special locations.
+assign_positions()
+
+board_size=len(positions)
+#initialize the players
+num_players = 2
+players = [{"name": "Player 1", "position": 0, "money": 1000, "properties": []},
+{"name": "Player 2", "position": 0, "money": 1000, "properties": []}]
+
+
+#initialize the dice
+dice = [1, 2, 3, 4, 5]
+
+ #start the game
+for player in players:
+# roll the dice
+    roll = random[dice]
+    player["position"] = (player["position"] + roll) 
+
+
+    # pay rent 
+    # if player["position"] is equal to foreign owned property
+        # reduce this player's money
+        # have another player's money go up
+
+    #                 owner = properties[player["position"]]["owner"]
+#                     if owner != player["name"]:
+#                     rent = properties[player["position"]]["rent"]
+#                     player["money"] -= rent
+#                     owner_index = [i for i in range(len(players)) if players[i]["name"] == owner][0]
+#                     players[owner_index]["money"] += rent
+#                     print(f"{player['name']} has paid {rent} to {owner}.")
+
+    # if its a special property
+    # activate choices/effects    
+        # set up random choices for the AI 
+
+    # if the property is free
+        # option to buy
+
+#         elif properties[player["position"]]["owner"] is None:
+#             property_name = properties[player["position"]]["name"]
+#             property_price = properties[player["position"]]["price"]
+#             if player["money"] >= property_price:
+#                 buy_property = input(f"{player['name']}, do you want to buy {property_name} for {property_price}? (y/n) ")
+#                 if buy_property.lower() == "y":
+#                     player["properties"].append(player["position"])
+#                         player["money"] -= property_price
+#                         properties[player["position"]]["owner"] = player["name"]
+#                         print(f"{player['name']} has bought {property_name}.")
+
+
+
+    # have an exit condition where a player runs out of money and they are kicked out
+
+    # AI variables - mixture of the below 0-1 effects
+        # Given immoral choices more likely to take them
+        # Aggressive buyer
+        # Aggresive builder
+        # 
